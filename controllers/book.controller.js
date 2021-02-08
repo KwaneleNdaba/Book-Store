d170cc4c6c99c9400498736cbe85562615f349b4
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2021-02-08 14:00:00 +0200

    routes added

---
+++ controllers/book.controller.js
@@ -0,0 +1,22 @@
+const bookModel = require("../models/book.model")
+
+const getAllbooks = async (req, res) => {
+    
+}
+
+const getSingleBookById = async (req, res) => {
+    
+}
+
+const addNewBook = async (req, res) => {
+    
+}
+const updateBook = async (req, res) => {
+    
+}
+
+const deleteBook = async (req, res) => {
+    
+}
+
+module.exports(getAllbooks,getSingleBookById,addNewBook, deleteBook, updateBook)
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const {getAllbooks,getSingleBookById,addNewBook, deleteBook, updateBook} = require("../controllers/book.controller")
 
 
 const router = express.Router();
 
-router.get("/books",);
-router.get("/books/:id",);
-router.post("/addBook",);
-router.put("/updateBook/:id",);
-router.delete("/deleteBook/:id",);
+router.get("/books",getAllbooks);
+router.get("/books/:id",getSingleBookById);
+router.post("/addBook",addNewBook);
+router.put("/updateBook/:id",updateBook);
+router.delete("/deleteBook/:id",deleteBook);
